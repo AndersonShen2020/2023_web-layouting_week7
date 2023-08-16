36156791bae9857be7657b156b67c69cdca6c36e
AndersonShen <shen.andersonshen@gmail.com>
2023-08-16 15:37:11 +0800

    feat: 增加 header 主架構

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,47 @@
 <template>
   <header>
-    header
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <form class="d-flex">
+            <input
+              class="form-control"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink class="nav-link active" aria-current="page" to="/"
+                  ><i class="icofont-ui-file"></i> REGISTER</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/"
+                  ><i class="icofont-ui-user"></i> LOGIN</RouterLink
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   </header>
 </template>
