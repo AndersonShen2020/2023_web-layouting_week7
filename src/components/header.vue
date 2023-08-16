6ba15743114b57793227fa670f6951ee66fcf505
AndersonShen <shen.andersonshen@gmail.com>
2023-08-16 20:54:56 +0800

    feat: 增加 PC 版 Header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,18 +1,29 @@
 <template>
   <header>
     <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <h1 class="me-5">
+            <RouterLink class="d-flex align-items-center" to="/">
+              <img
+                class="img-fluid me-3 logo"
+                src="@/assets/img/logo.png"
+                alt=""
+              />
+              <p class="fs-2 d-flex align-items-center">
+                7TAO<span class="noto-tc-serif fs-6">活動訂票網</span>
+              </p>
+            </RouterLink>
+          </h1>
           <form class="d-flex">
             <input
               class="form-control"
               type="search"
-              placeholder="Search"
+              placeholder="探索活動"
               aria-label="Search"
             />
-            <button class="btn btn-outline-success" type="submit">
-              Search
+            <button class="btn btn-primary" type="submit">
+              <i class="icofont-search-1"></i>
             </button>
           </form>
           <button
@@ -29,7 +40,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <RouterLink class="nav-link active" aria-current="page" to="/"
+                <RouterLink class="nav-link" aria-current="page" to="/"
                   ><i class="icofont-ui-file"></i> REGISTER</RouterLink
                 >
               </li>
