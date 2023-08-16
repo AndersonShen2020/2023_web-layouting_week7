8175871f4973c0e46240e3ccbadc5a528a2db698
AndersonShen <shen.andersonshen@gmail.com>
2023-08-16 14:38:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# 2023 網頁切版直播班 - 第四週 - 多頁式 RWD 網站
+
+## Node.js
+
+使用 v19.6.0
+
+## 設計稿
+
+> <https://xd.adobe.com/view/5b20cbc4-5c64-4b67-814e-633b078a8cd4-0e73/grid/>
---
+++ src/views/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <p class="test">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, enim neque
+      eligendi, et, ipsum aperiam eaque quasi voluptatem placeat consequatur
+      exercitationem delectus! Minima eius maiores, ducimus ipsa distinctio
+      ullam voluptatibus?
+    </p>
+  </main>
+</template>
