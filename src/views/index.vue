0d09dee2b283dce41a35084348e41a63c05aa826
AndersonShen <shen.andersonshen@gmail.com>
2023-08-18 16:28:19 +0800

    feat: 增加 swiper 幻燈片

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,82 @@
+<script setup>
+// import Swiper core and required modules
+import { Navigation } from "swiper/modules"
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/navigation"
+
+const modules = [Navigation]
+</script>
+
 <template>
   <main>
-    <p class="test">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, enim neque
-      eligendi, et, ipsum aperiam eaque quasi voluptatem placeat consequatur
-      exercitationem delectus! Minima eius maiores, ducimus ipsa distinctio
-      ullam voluptatibus?
-    </p>
+    <!-- 隔牆有耳-沒有草東的派對 -->
+    <div class="container">
+      <Swiper
+        :modules="modules"
+        :slides-per-view="1"
+        :space-between="50"
+        navigation
+      >
+        <SwiperSlide
+          ><img src="../assets/img/event/01.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/02.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/03.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/04.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/05.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/06.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/07.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/08.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/09.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/10.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/11.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/12.jpg" alt=""
+        /></SwiperSlide>
+        <SwiperSlide
+          ><img src="../assets/img/event/13.jpg" alt=""
+        /></SwiperSlide>
+      </Swiper>
+    </div>
+
+    <!-- NAV -->
+    <div class="container"></div>
+
+    <!-- RECENT 近期活動 -->
+    <div class="container"></div>
+
+    <!-- ONLINE 線上活動 -->
+    <div class="container"></div>
+
+    <!-- 活動集錦・ALBUMS -->
+    <div class="container"></div>
+
+    <!-- FOLLOW subscribe -->
+    <div class="container"></div>
   </main>
 </template>
