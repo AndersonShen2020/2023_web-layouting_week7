a36795393a4dd1995aa20776987212fdc5d50ac0
AndersonShen <shen.andersonshen@gmail.com>
2023-08-31 20:46:39 +0800

    update: 調整 border

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer class="text-center">
+  <footer
+    class="text-center border border-3 border-start-0 border-end-0 border-bottom-0 border-primary"
+  >
     <p class="py-md-8 py-7">© 7TAO七逃. All Rights Reserved</p>
   </footer>
 </template>
